b4ba5ec7f2444b34e2a2884979bfced34240b941
Ryan Bucshon <busheezy@gmail.com>
2019-01-18 17:27:07 -0600

    remove extra git

--- sh/init.sh
+++ sh/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 METAMOD_VERSION=1.10
 METAMOD_VERSION_FULL=1.10.7
-METAMOD_BUILD=git968
+METAMOD_BUILD=968
 
 echo "Downloading metamod"
 wget https://mms.alliedmods.net/mmsdrop/${METAMOD_VERSION}/mmsource-${METAMOD_VERSION_FULL}-git${METAMOD_BUILD}-linux.tar.gz
