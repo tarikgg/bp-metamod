5a60b4436d018e58ebd932fcb230f6fae765ba07
Ryan Bucshon <busheezy@gmail.com>
2019-01-18 18:48:38 -0600

    revert

--- sh/init.sh
+++ sh/init.sh
@@ -6,8 +6,8 @@ METAMOD_BUILD=968
 echo "Downloading metamod"
 wget https://mms.alliedmods.net/mmsdrop/${METAMOD_VERSION}/mmsource-${METAMOD_VERSION_FULL}-git${METAMOD_BUILD}-linux.tar.gz
 echo "Downloaded metamod"
-mkdir -p "${SERVER_PATH}"
+mkdir -p ${serverPath}
 tar -xf "mmsource-${METAMOD_VERSION_FULL}-git${METAMOD_BUILD}-linux.tar.gz -C ${serverPath}"
-echo "${SERVER_PATH}"
+echo "${serverPath}"
 echo "extracting metamod"
 rm mmsource-${METAMOD_VERSION_FULL}-git${METAMOD_BUILD}-linux.tar.gz
