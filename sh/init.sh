c926fbc3145940c7014a11856297f1b8ef0b4d78
Ryan Bucshon <busheezy@gmail.com>
2019-01-18 21:13:38 -0600

    add word

--- sh/init.sh
+++ sh/init.sh
@@ -13,4 +13,4 @@ echo "Downloaded metamod"
 tar -xf mmsource-${METAMOD_VERSION_FULL}-git${METAMOD_BUILD}-linux.tar.gz -C ${CSGO_PATH}
 echo "extracting metamod"
 rm mmsource-${METAMOD_VERSION_FULL}-git${METAMOD_BUILD}-linux.tar.gz
-echo "deleted archive"
+echo "deleted metamod archive"
