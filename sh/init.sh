a0eb36a4389f62bc5a59d862c834e865fc450f2f
Ryan Bucshon <busheezy@gmail.com>
2019-01-18 20:41:35 -0600

    test

--- sh/init.sh
+++ sh/init.sh
@@ -3,12 +3,15 @@ METAMOD_VERSION=1.10
 METAMOD_VERSION_FULL=1.10.7
 METAMOD_BUILD=968
 
+CSGO_PATH="${serverPath}/csgo"
+
+mkdir "${serverPath}"
+mkdir "${CSGO_PATH}"
+
 echo "Downloading metamod"
 wget https://mms.alliedmods.net/mmsdrop/${METAMOD_VERSION}/mmsource-${METAMOD_VERSION_FULL}-git${METAMOD_BUILD}-linux.tar.gz
 echo "Downloaded metamod"
-mkdir "${serverPath}"
-mkdir "${serverPath}/csgo"
-tar -xf mmsource-${METAMOD_VERSION_FULL}-git${METAMOD_BUILD}-linux.tar.gz -C ${serverPath}/csgo
+tar -xf mmsource-${METAMOD_VERSION_FULL}-git${METAMOD_BUILD}-linux.tar.gz -C ${CSGO_PATH}
 echo "extracting metamod"
 rm mmsource-${METAMOD_VERSION_FULL}-git${METAMOD_BUILD}-linux.tar.gz
 echo "deleted archive"
