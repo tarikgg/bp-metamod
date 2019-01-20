2ff2b79a20fa1f745971a6f08493b6285bc049ec
Ryan Bucshon <busheezy@gmail.com>
2019-01-20 01:15:58 -0600

    change vars

--- sh/init.sh
+++ sh/init.sh
@@ -3,7 +3,7 @@ METAMOD_VERSION=1.10
 METAMOD_VERSION_FULL=1.10.7
 METAMOD_BUILD=968
 
-CSGO_PATH="${SERVER_PATH}/csgo"
+CSGO_PATH="${sb_serverPath}/csgo"
 
 echo "Downloading metamod"
 wget https://mms.alliedmods.net/mmsdrop/${METAMOD_VERSION}/mmsource-${METAMOD_VERSION_FULL}-git${METAMOD_BUILD}-linux.tar.gz
