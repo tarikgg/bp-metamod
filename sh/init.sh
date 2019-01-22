57773844e3011449729b84f54c3cffd1930ce454
Ryan Bucshon <busheezy@gmail.com>
2019-01-22 11:57:03 -0600

    quiet

--- sh/init.sh
+++ sh/init.sh
@@ -4,6 +4,6 @@ METAMOD_VERSION_FULL=1.10.7
 METAMOD_BUILD=968
 CSGO_PATH="${bp_serverPath}/csgo"
 FILE_NAME="mmsource-${METAMOD_VERSION_FULL}-git${METAMOD_BUILD}-linux.tar.gz"
-wget -nv https://mms.alliedmods.net/mmsdrop/${METAMOD_VERSION}/${FILE_NAME}
+wget -q -nv https://mms.alliedmods.net/mmsdrop/${METAMOD_VERSION}/${FILE_NAME}
 tar -xf ${FILE_NAME} -C ${CSGO_PATH}
 rm ${FILE_NAME}
