d685a691ebe4f974ee8adec7f30285c40eacb847
Ryan Bucshon <busheezy@gmail.com>
2019-03-28 22:10:53 -0500

    update

--- sh/init.sh
+++ sh/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 METAMOD_VERSION=1.10
 METAMOD_VERSION_FULL=1.10.7
-METAMOD_BUILD=968
+METAMOD_BUILD=970
 CSGO_PATH="${bp_serverPath}/csgo"
 FILE_NAME="mmsource-${METAMOD_VERSION_FULL}-git${METAMOD_BUILD}-linux.tar.gz"
 wget -q -nv https://mms.alliedmods.net/mmsdrop/${METAMOD_VERSION}/${FILE_NAME}
