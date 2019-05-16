26fce6c0f0a882588c5c64d5113b19f15a5a8d5e
Ryan Bucshon <busheezy@gmail.com>
2019-05-16 15:11:57 -0500

    use mod dir

--- sh/init.sh
+++ sh/init.sh
@@ -2,8 +2,7 @@
 METAMOD_VERSION=1.10
 METAMOD_VERSION_FULL=1.10.7
 METAMOD_BUILD=970
-CSGO_PATH="${bp_dir}/csgo"
 FILE_NAME="mmsource-${METAMOD_VERSION_FULL}-git${METAMOD_BUILD}-linux.tar.gz"
 wget -q -nv https://mms.alliedmods.net/mmsdrop/${METAMOD_VERSION}/${FILE_NAME}
-tar -xf ${FILE_NAME} -C ${CSGO_PATH}
+tar -xf ${FILE_NAME} -C ${bp_modDir}
 rm ${FILE_NAME}
