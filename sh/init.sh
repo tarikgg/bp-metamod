1b95a87cf8e75b9437062cd951ec1721d9dc5d4e
Ryan Bucshon <busheezy@gmail.com>
2019-10-13 13:11:44 -0500

    try dis

--- sh/init.sh
+++ sh/init.sh
@@ -3,6 +3,8 @@ METAMOD_VERSION=1.10
 METAMOD_VERSION_FULL=1.10.7
 METAMOD_BUILD=970
 FILE_NAME="mmsource-${METAMOD_VERSION_FULL}-git${METAMOD_BUILD}-linux.tar.gz"
-wget -q -nv https://mms.alliedmods.net/mmsdrop/${METAMOD_VERSION}/${FILE_NAME}
+URL="https://mms.alliedmods.net/mmsdrop/${METAMOD_VERSION}/${FILE_NAME}"
+echo "${URL}"
+wget -q -nv "${URL}"
 tar -xf ${FILE_NAME} -C ${bp_modDir}
 rm ${FILE_NAME}
